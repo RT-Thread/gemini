4a2991c6779d0e55cd4ce25bd497f06ddd26755c
RT-Thread <master@rt-thread.com>
2015-06-21 16:04:38 +0800

    Update env.sh

--- env.sh
+++ env.sh
@@ -4,7 +4,8 @@ export RTT_ROOT=/home/coding/workspace/rt-thread
 export RTT_CC=gcc
 export RTT_EXEC_PATH=/home/coding/workspace/arm-2012.09/bin
 
-export KDIR=/home/coding/workspace/linux-3.18.16
+# export KDIR=/home/coding/workspace/linux-3.18.16
+export KDIR=/home/coding/workspace/buildroot-2015.05/output/build/linux-custom
 export PATH=/home/coding/workspace/gcc-linaro-4.9-2014.11-x86_64_arm-linux-gnueabi/bin:$PATH
-export CROSS_COMPILE=arm-linux-gnueabihf-
+export CROSS_COMPILE=arm-linux-gnueabi-
 export ARCH=arm
